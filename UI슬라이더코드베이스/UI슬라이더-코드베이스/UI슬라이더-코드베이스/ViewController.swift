7c2605929bd35fc598fbea59c6f3bf17653328ae
jwl-98 <apple_komputer@icloud.com>
2024-11-30 23:03:00 +0900

    initial commit

---
+++ UI슬라이더코드베이스/UI슬라이더-코드베이스/UI슬라이더-코드베이스/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI슬라이더-코드베이스
+//
+//  Created by 진욱의 Macintosh on 11/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
