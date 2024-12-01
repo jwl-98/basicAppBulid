30b5ff05accd90112e0bd1851fb858eb2b4cec85
jwl-98 <apple_komputer@icloud.com>
2024-12-01 21:11:09 +0900

    DiceGame-CodeBase

---
+++ 1-주사위게임-코드베이스/1-주사위게임-코드베이스/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1-주사위게임-코드베이스
+//
+//  Created by 진욱의 Macintosh on 12/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
